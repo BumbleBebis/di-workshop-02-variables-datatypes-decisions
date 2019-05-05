6346345c6b371e7a93a56dc9a77500764d272930
Emma Van Eeckhout <emmavaneeckhout@hotmail.com>
2019-05-05 23:23:06 +0100

    added bounciness and counter

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,6 +1,10 @@
-var position = 100
+var position = 50
 var name = 'Emma'
 var moving = true
+var direction = '+'
+var score = 0
+
+
 
 function setup() {
   createCanvas(400, 200)
@@ -9,14 +13,31 @@ function setup() {
 function draw() {
   background(100)
   if (moving){
-    position = position  +1
+    if (direction === '+') {
+      position = position + 1
+    } else if (direction === '-') {
+      position = position - 1
+    }
   }
-  rect(position, 50, 10, 10)
-  text('My name is' + ' ' + name, 10, 30)
+
+  rect(position, 100, 10, 10)
+  text('My name is ' + name, 10, 30)
+  text('Score: ' + score , 10, 60)
   console.log(moving)
+
+
+  if (position > 390){
+    direction = '-'
+    score++
+  }
+
+  if (position < 0){
+    direction = '+'
+    score++
+  }
 }
 
-  function mousePressed() {
-    moving = !moving
-  }
 
+function mousePressed() {
+  moving = !moving
+}
